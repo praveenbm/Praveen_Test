a24b5da1ae0ead451303bd7bed9d6ca68a80ef3d
shivask <shivakumar.shyavi@emc.com>
2012-05-10 12:26:07 +0530

    test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+
+namespace OverrideAndNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("\nTestCars4");
+            System.Console.WriteLine("----------");
+            Car car2 = new ConvertibleCar();
+            Car car3 = new Minivan();
+
+            //car2.ShowDetails();
+            //car3.ShowDetails();
+
+            Car car4 = new derivedConvertibleCar();
+            
+            car4.ShowDetails();
+            
+
+
+
+
+            Console.Read();
+        }
+    }
+
+    // Define the base class, Car. The class defines two methods,
+    // DescribeCar and ShowDetails. DescribeCar calls ShowDetails, and each derived
+    // class also defines a ShowDetails method. The example tests which version of
+    // ShowDetails is selected, the base class method or the derived class method.
+    class Car
+    {
+        public void DescribeCar()
+        {
+            System.Console.WriteLine("Four wheels and an engine.");
+            ShowDetails();
+        }
+
+        public virtual void ShowDetails()
+        {
+            System.Console.WriteLine("Standard transportation.");
+        }
+    }
+
+    class derivedConvertibleCar : ConvertibleCar
+    {
+        public new void ShowDetails()
+        {
+            base.ShowDetails();
+            System.Console.WriteLine("Derived Convertible");
+        }
+    }
+    // Define the derived classes.
+
+    // Class ConvertibleCar uses the new modifier to acknowledge that ShowDetails
+    // hides the base class method.
+    class ConvertibleCar : Car
+    {
+        public override void ShowDetails()
+        {
+            System.Console.WriteLine("A roof that opens up.");
+        }
+    }
+
+    // Class Minivan uses the override modifier to specify that ShowDetails
+    // extends the base class method.
+    class Minivan : Car
+    {
+        public override void ShowDetails()
+        {
+            System.Console.WriteLine("Carries seven people.");
+        }
+    }
+
+
+    class BaseClass
+    {
+        public virtual  void Method1()
+        {
+            Console.WriteLine("Base - Method1");
+        }
+
+        public virtual void Method2()
+        {
+            Console.WriteLine("Base - Method2");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("Derived - Method1");
+        }
+
+        public  new void Method2()
+        {
+            Console.WriteLine("Derived - Method2");
+        }
+    }
+}
